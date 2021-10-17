f414a1fc2764ed69fe362b80bf8492b304f4dd92
Luidmila Trach <ljudmila.trach@privatbank.ua>
2021-10-18 00:40:42 +0300

    init commit

--- src/com/pb/Trach/hw2/Calculator.java
+++ src/com/pb/Trach/hw2/Calculator.java
@@ -1,5 +1,6 @@
 package com.pb.Trach.hw2;
 
+
 import java.util.Scanner;
 
 public class Calculator {
@@ -7,29 +8,35 @@ public class Calculator {
         Scanner enter = new Scanner(System.in);
         int operand1;
         int operand2;
-        System.out.println("1");
+        System.out.println("ввести цифру №1");
         operand1 = enter.nextInt();
-        System.out.println("2");
+        System.out.println("ввести цифру №2");
         operand2 = enter.nextInt();
-        System.out.println("+");
+        System.out.println("ввести знак +");
         char sign = enter.next().charAt(0);
         int operand;
         switch (sign) {
             case '+':
                 operand = operand1 + operand2;
-                System.out.println("11: " + operand);
+                System.out.println("ответ 11: " + operand);
                 break;
             case '-':
                 operand = operand1 - operand2;
-                System.out.println("7: " + operand);
+                System.out.println(" ответ 7: " + operand);
                 break;
             case '*':
                 operand = operand1 * operand2;
-                System.out.println("18: " + operand);
+                System.out.println("ответ 18: " + operand);
                 break;
             case '/':
-                operand = operand1 / operand2;
-                System.out.println("3,5: " + operand);
-                break; }
+                if (operand2 == 0) {
+                    System.out.println(" нельзя делить на 0 ");
+                    break;
+                } else {
+                    operand = operand1 / operand2;
+                    System.out.println("ответ 3,5: " + operand);
+                    break;
                 }
+        }
+    }
 }
